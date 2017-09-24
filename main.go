2a5ed2284ec60962365178d66a7869b095fbe3ff
Anton Artemov <anton.artemov2011@yandex.ru>
2017-09-24 22:49:21 +0300

    Some refactoring.
    Just added init function.

--- main.go
+++ main.go
@@ -9,31 +9,34 @@ import (
 	"web-application/xml_validator"
 )
 
-func main() {
-	var (
-		configname  string
-		xsdFilename string
-		log         string
-	)
+var (
+	configname  string
+	xsdFilename string
+	log         string
+)
+
+func init() {
 	flag.StringVar(&configname, "config", "config.xml", "name of config file")
 	flag.StringVar(&xsdFilename, "xsd", "xsd", "name of xsd file")
 	flag.StringVar(&log, "log", "log.txt", "name of log file")
 
 	flag.Parse()
+}
 
+func main() {
 	var logger = logger.Create(nil, logger.Debug|logger.Info, logger.DateTime|logger.Shortfile)
 	defer logger.Close()
 	logger.AddOutput(os.Stdout)
 	logger.AddOutputFile(log)
-	logger.Debug("wad")
 
 	err := xml_validator.Validate(configname, xsdFilename)
 	if err != nil {
-		fmt.Println(err)
+		logger.Info(err)
 	}
+
 	configuration, err := xml_parser.Parse(configname)
 	if err != nil {
-		fmt.Println(err)
+		logger.Info(err)
 	}
 
 	fmt.Printf("%v\n", configuration.Database[0].User)
