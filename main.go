8230da013d0420448987de5a6ba1b97fd45db068
Anton Artemov <anton.artemov2011@yandex.ru>
2017-10-03 21:50:37 +0300

    Some refactoring.
    Change variable names.

--- main.go
+++ main.go
@@ -2,42 +2,42 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"web-application/logger"
 	"web-application/xml_parser"
 	"web-application/xml_validator"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 var (
-	configname  string
-	xsdFilename string
-	log         string
+	configFilename string
+	xsdFilename    string
+	logFilename    string
 )
 
 func init() {
-	flag.StringVar(&configname, "config", "config.xml", "name of config file")
+	flag.StringVar(&configFilename, "config", "config.xml", "name of config file")
 	flag.StringVar(&xsdFilename, "xsd", "xsd", "name of xsd file")
-	flag.StringVar(&log, "log", "log.txt", "name of log file")
+	flag.StringVar(&logFilename, "log", "log.txt", "name of log file")
 
 	flag.Parse()
 }
 
 func main() {
-	var logger = logger.Create(nil, logger.Debug|logger.Info, logger.DateTime|logger.Shortfile)
+	var logger = logger.Initialize("[WEB APP]", logger.Debug|logger.Info, logger.DateTime|logger.Shortfile)
 	defer logger.Close()
 	logger.AddOutput(os.Stdout)
-	logger.AddOutputFile(log)
-
-	err := xml_validator.Validate(configname, xsdFilename)
+	logger.AddOutputFile(logFilename)
+	err := xml_validator.Validate(configFilename, xsdFilename)
 	if err != nil {
 		logger.Info(err)
 	}
 
-	configuration, err := xml_parser.Parse(configname)
+	configuration, err := xml_parser.Parse(configFilename)
 	if err != nil {
 		logger.Info(err)
 	}
 
-	fmt.Printf("%v\n", configuration.Database[0].User)
+	spew.Dump(configuration)
 }
