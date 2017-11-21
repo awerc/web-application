0e8b64cab08c4af4d525461efbe52618fa7cd7af
Anton Artemov <anton.artemov2011@yandex.ru>
2017-11-21 18:13:02 +0300

    Added interaction with database.

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	// "github.com/davecgh/go-spew/spew"
 	"os"
+	"web-application/database"
 	"web-application/logger"
 	"web-application/xml_parser"
 	"web-application/xml_validator"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 var (
@@ -25,6 +26,16 @@ func init() {
 	flag.Parse()
 }
 
+func check_error(err error, terminate bool, logger *logger.Logger) {
+	if err != nil {
+		logger.Info(err)
+		if terminate {
+			os.Exit(1)
+		}
+	}
+	return
+}
+
 func main() {
 	var logger = logger.Initialize("[WEB APP]", logger.Debug|logger.Info, logger.DateTime|logger.Shortfile)
 	defer logger.Close()
@@ -32,16 +43,54 @@ func main() {
 	logger.AddOutputFile(logFilename)
 
 	configuration, err := xml_parser.Parse(configFilename)
-	if err != nil {
-		logger.Info(err)
-		os.Exit(1)
-	}
+	check_error(err, true, logger)
 
 	err = xml_validator.Validate(configuration)
-	if err != nil {
-		logger.Info(err)
-		os.Exit(1)
+	check_error(err, true, logger)
+
+	db, err := food_delivery.Connect(configuration)
+	check_error(err, true, logger)
+	defer db.Close()
+
+	err = db.Ping()
+	check_error(err, true, logger)
+
+	orders, err := food_delivery.GetOrderForClient(db, 1)
+	check_error(err, true, logger)
+
+	fmt.Println("\n------------Orders for 1st client-------------")
+	for _, element := range orders {
+		element.Println()
 	}
 
-	spew.Dump(configuration)
+	products, err := food_delivery.GetAllProducts(db, 1)
+	check_error(err, true, logger)
+
+	fmt.Println("\n-----------All avlailable products--------------")
+	for _, element := range products {
+		element.Println()
+	}
+
+	costs, err := food_delivery.GetCostOfAllOrdersForClient(db, 1)
+	check_error(err, true, logger)
+
+	fmt.Println("\n--------Total cost of 1st client orders----------")
+	for _, element := range costs {
+		element.Println()
+	}
+
+	clients, err := food_delivery.GetAllClients(db)
+	check_error(err, true, logger)
+
+	fmt.Println("\n----------------Clients-----------------")
+	for _, element := range clients {
+		fmt.Printf("name: %s tel: %s bday: %s\n", element.Name, element.Tel, element.Bday.Format("2 Jan 2006"))
+	}
+
+	// err = food_delivery.AddClient(db, "login7", "pass", "88005553535", "1991-01-22", "client8")
+	// check_error(err, true, logger)
+
+	// err = food_delivery.ChangeClientTelephone(db, 7, "877454887")
+	// check_error(err, true, logger)
+	//spew.Dump(configuration)
 }
